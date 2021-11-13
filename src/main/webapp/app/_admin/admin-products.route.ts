2ffc656391bc744fb836ed277b3bbc7a01defa70
Kabelo Maroga <kabelo@psybergate.co.za>
2021-11-13 16:44:42 +0200

    fixed the cart badge.

--- src/main/webapp/app/_admin/admin-products.route.ts
+++
@@ -1,8 +0,0 @@
-import { Route } from '@angular/router';
-
-import { AdminProductsComponent } from './admin-products.component';
-
-export const ADMIN_PRODUCTS_ROUTE: Route = {
-  path: 'admin-products',
-  component: AdminProductsComponent,
-};
--- src/main/webapp/app/bs-navbar/bs-navbar.module.ts
+++ src/main/webapp/app/bs-navbar/bs-navbar.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { NAV_BAR_ROUTE } from './bs-navbar.route';
 import { BsNavbarComponent } from './bs-navbar.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([NAV_BAR_ROUTE])],
+  imports: [SharedModule, RouterModule.forChild([NAV_BAR_ROUTE]), ButtonModule],
   declarations: [BsNavbarComponent],
   exports: [BsNavbarComponent],
 })
